f8ac0c33bef1d4b65598336425ecce18786189c1
amer <amer.mriziq@sjsu.edu>
2022-04-19 18:04:16 -0700

    add resume redirect (mriziq.com/resume)

--- next.config.js
+++ next.config.js
@@ -2,5 +2,22 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["media.discordapp.net"],
+    webpackDevMiddleware: config => {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      }
+
+      return config
+    }
+  },
+  async redirects() {
+    return [
+      {
+        source: "/resume",
+        destination: "/files/my-resume.pdf",
+        permanent: true,
+      },
+    ]
   },
 }
