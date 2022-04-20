72318b59bdcd72e1b30403e9cfc2160d1c6b5f7c
amer <amer.mriziq@sjsu.edu>
2022-04-19 18:55:31 -0700

    add 404 page

---
+++ pages/404.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Box, VStack, Center, Heading, Text, Button } from "@chakra-ui/react"
+import Link from "next/link"
+import styles from '../styles/Home.module.css'
+
+export default function Resolve404() {
+    return (
+        <Center className={styles.container}>
+            <VStack pt="6rem" maxW="100vw" maxH="100vh" textAlign="center" spacing={5} paddingBottom={"35px"}>
+                <Heading fontSize={128}>Oops!</Heading>
+                <Heading fontWeight="extrabold">This Page Doesn't Exist</Heading>
+                <Text fontWeight="semibold">You don't have to go home, but you shouldn't stay here.</Text>
+                <Box pt="1rem">
+                    <Link href="/" passHref>
+                        <Button px="1.5rem" variant="solid" colorScheme={"red"}> GO SAFE ZONE</Button>
+                    </Link>
+                </Box>
+            </VStack>
+        </Center>
+    )
+}
